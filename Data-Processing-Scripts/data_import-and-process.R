fbd905963e3464e409cde8790480ec5a66feb731
Robert Johnson <johnson.robert.333@gmail.com>
2026-02-06 15:27:32 -0600

    undo porewater name change

--- Data-Processing-Scripts/data_import-and-process.R
+++ Data-Processing-Scripts/data_import-and-process.R
@@ -125,7 +125,7 @@ epiphytes = epiphytes_raw %>%
 porewater_raw = read_csv("MSI25_experiment_porewater_samples.csv")
 
 # Initial cleaning
-porewater_dat = porewater_raw %>%
+porewater = porewater_raw %>%
    janitor::remove_empty(which="rows") %>%
    mutate(week = paste0("w", week),
           date = mdy(date))
