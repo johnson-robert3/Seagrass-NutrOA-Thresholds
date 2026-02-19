e47af134f74160dbc2ae26acfda5cdbf7061ea3c
Robert Johnson <johnson.robert.333@gmail.com>
2026-02-19 14:35:38 -0600

    add small run of wk 9 reruns

--- Data-Processing-Scripts/data_sulfide-spec-data.R
+++ Data-Processing-Scripts/data_sulfide-spec-data.R
@@ -170,13 +170,34 @@ sulf_wk9 = calc_vial_S(sulf_wk9, raw_sulf_wk9, std_dec25)
 sulf_wk9 = sulf_wk9 %>% anti_join(wk9_reruns)
 
 
+#- Run 5: Week 9 sample reruns -#
+# Standard curve to use: Dec. 2025
+
+# raw data
+raw_sulf_wk9_reruns = read_csv("Sulfide spectrophotometer data/2026.02.06 - porewater sulfide_wk9_reruns.csv") %>%
+   janitor::remove_empty(which = 'rows')
+
+# check measured concentration of standards
+check_stds(raw_sulf_wk9_reruns, std_dec25)  # a bit off at the low end (L2) 
+
+# Pre-process data sheets, remove unnecessary data/rows
+sulf_wk9_reruns = rm_zbsc(raw_sulf_wk9_reruns)
+
+# check agreement between sample dupes
+sulf_wk9_reruns %>% filter(str_detect(sample_id, "dup") | lead(str_detect(sample_id, "dup")))
+
+# Sulfide concentration in vials (units = uM)
+sulf_wk9_reruns = calc_vial_S(sulf_wk9_reruns, raw_sulf_wk9_reruns, std_dec25)
+   # still missing two samples (T2-H151-w9, T2-H159-w9) that weren't included in the rerun for some reason...
+
+
 
 #--
 # Sulfide Concentration
 #--
 
 # Combine spec runs and calculate sulfide concentration (units = uM)
-sulfide = bind_rows(sulf_wk2, sulf_wk3, sulf_wk6, sulf_wk9) %>%
+sulfide = bind_rows(sulf_wk2, sulf_wk3, sulf_wk6, sulf_wk9, sulf_wk9_reruns) %>%
    # correct measured sulfide concentration for any dilution prior to adding diamine reagent (units = uM)
    mutate(scint_S_uM = vial_S_uM * dilution_pre) %>%
    # for samples below DL at dilution 1:2 (abs < 0.02), vial_S becomes 2.0, but should be 1.0uM, replace conc with half the DL (DL=2uM)
